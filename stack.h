5794f5805b23f7cc289ba389d016429084c7d978
HunterPlus <zlw08140957@gmail.com>
2021-02-25 08:40:23 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -6,6 +6,14 @@ struct stack {
   char *data;
 };
 
+struct stack *st_create(size_t size){
+  struct stack *st;
+  st = (struct stack *)malloc(sizeof(stack));
+  st->size = size;
+  st->top = -1;
+  st->data = (char *)malloc(size);
+  return st;
+}
 void push(const struct stack *st, char c){
   
 }
