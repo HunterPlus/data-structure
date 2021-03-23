cb5a9d9a0271456a1d7032d5e07fed12a12731eb
HunterPlus <zlw08140957@gmail.com>
2021-03-23 14:04:43 +0800

    Update avl.c

--- tree/avl.c
+++ tree/avl.c
@@ -24,6 +24,13 @@ struct Node* newNode(int key)
     
     return node;
 }
+/*
+                y                               x
+               / \     Right Rotation          /  \
+              x   T3   – - – - – - – >        T1   y
+             / \       < - - - - - - -            / \
+            T1  T2     Left Rotation            T2  T3
+*/
 struct Node* rightRotate (struct Node* y)
 {
     struct Node *x = y->left;
