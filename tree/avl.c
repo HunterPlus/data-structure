a3d1f7c65d5ea92803d57d5362b10b4532cd4b2d
HunterPlus <zlw08140957@gmail.com>
2021-03-23 18:35:18 +0800

    Update avl.c

--- tree/avl.c
+++ tree/avl.c
@@ -52,9 +52,9 @@ struct Node* leftRotate (struct Node* x)
     y->left = x;
     x->right = T2;
     
-    y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
-    
+    y->height = max(height(y->left), height(y->right)) + 1;
+        
     return y;
 }
 int getBalance(struct Node* N)
