15a41e71272c150ffb0101532e5fe4e6716d3396
HunterPlus <zlw08140957@gmail.com>
2021-03-23 20:42:40 +0800

    Update avl.c

--- tree/avl.c
+++ tree/avl.c
@@ -96,6 +96,90 @@ struct Node *insert(struct Node* node, int key)
     
     return node;
 }
+
+struct Node *minNode (struct Node *node)
+{
+    struct Node *current = node;
+    
+    while (current->left != NULL)
+        current = current->left;
+    
+    return current;
+}
+
+struct Node *deleteNode (struct Node *root, int key)
+{
+    if (root == NULL)
+        return root;
+    
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+        
+    else
+    {
+        if (root->left == NULL || root->right == NULL)
+        {
+            struct Node *t = root->left? root->left : root->right;
+            
+            if (t == NULL)
+            {
+                t = root;
+                root = NULL;
+            }
+            else
+                *root = *t;
+                
+            free (t);
+        }
+        else
+        {
+            struct Node *t = minNode(root->right);
+            root->key = t->key;
+            root->right = deleteNode(root->right, t->key);
+        }
+    }
+    if (root == NULL)
+        return root;
+        
+    root->height = max(height(root->left), height(root->right)) + 1;
+    
+    int balance = getBalance(root);
+    
+    if (balance > 1 && getBalance(root->left) >= 0)
+        return rightRotate(root);
+    
+    if (balance < -1 && getBalance(root->right) <= 0)
+        return leftRotate(root);
+        
+    if (balance > 1 && getBalance(root->left) < 0)
+    {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    
+    if (balance < -1 && getBalance(root->right) > 0)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+    
+    return root;
+}
+
+struct Node *search (struct Node *node, int key)
+{
+    if (node == NULL || key == node->key)
+        return node;
+ 
+    if (key < node->key)
+        return search(node->left, key);
+    
+    return search(node->right, key);
+}
+
 void preOrder(struct Node *root)
 {
     if (root != NULL)
@@ -121,5 +205,51 @@ int main()
            " tree is \n");
     preOrder(root);  
 
+    return 0;
+}
+
+int main()
+{
+  struct Node *root = NULL;
+  
+    root = insert(root, 9);
+    root = insert(root, 5);
+    root = insert(root, 10);
+    root = insert(root, 0);
+    root = insert(root, 6);
+    root = insert(root, 11);
+    root = insert(root, -1);
+    root = insert(root, 1);
+    root = insert(root, 2);
+  
+    /* The constructed AVL Tree would be
+            9
+           /  \
+          1    10
+        /  \     \
+       0    5     11
+      /    /  \
+     -1   2    6
+    */
+  
+    printf("Preorder traversal of the constructed AVL "
+           "tree is \n");
+    preOrder(root);
+  
+    root = deleteNode(root, 10);
+  
+    /* The AVL Tree after deletion of 10
+            1
+           /  \
+          0    9
+        /     /  \
+       -1    5     11
+           /  \
+          2    6
+    */
+  
+    printf("\nPreorder traversal after deletion of 10 \n");
+    preOrder(root);
+  
     return 0;
 }
