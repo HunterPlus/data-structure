daa6f8e7a65ee74424d75b9929de543ea37b6ca9
HunterPlus <zlw08140957@gmail.com>
2021-04-02 09:02:16 +0800

    Update bfs.c

--- graph/bfs.c
+++ graph/bfs.c
@@ -12,16 +12,16 @@ void bfs (int g[V][V], int start)
     int visited[V] = {0};
     
     Q[++rear] = start;
+    visited[start] = 1;
     
     while (front != rear)
     {
         vertex = Q[++front];
         printf("vertex %d\n", vertex);
-        visited[vertex] = 1;
         
         for (i = 0; i < V; i++)
             if (visited[i] != 1 && g[vertex][i] != 0)
-                Q[++rear] = i;
+                Q[++rear] = i, visited[i] = 1;
     }
     
 }
@@ -34,7 +34,7 @@ int main()
                         { 6, 8, 0, 0, 9 },
                         { 0, 5, 7, 9, 0 } };
                         
-    bfs(graph, 2);
+    bfs(graph, 4);
 
     return 0;
 }
