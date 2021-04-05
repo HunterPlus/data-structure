bab5ad7673592e13a03c43f33de57230ae32bd08
HunterPlus <zlw08140957@gmail.com>
2021-04-05 14:04:10 +0800

    Update and rename string.c to length_and_reverse.c

---
+++ string/length_and_reverse.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int stringlength (char *s)
+{
+    int length;
+    for (length = 0; s[length] != '\0'; length++) ;
+    
+    return length;
+}
+
+void reverseString (char* s)
+{
+    int size;
+    for (size = 0; s[size]; size++) ;
+    
+    int i, j;
+    char c;
+    for (i=0,j=size-1; i < j; i++,j--)
+    {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+int main()
+{
+    char s[] = "Algorithms";
+    
+    reverseString(s);
+    
+    printf("%s\n", s);
+
+    return 0;
+}
+
