acb7946016a7c17cf094276c3ece11ea542236d9
HunterPlus <zlw08140957@gmail.com>
2021-04-06 13:39:02 +0800

    Update quicksort1.c

--- sorting/quicksort1.c
+++ sorting/quicksort1.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
-void swap (int *a, int *b) {int *t = *a; *a = *b; *b = *t; }
+void swap (int *a, int *b) {int t = *a; *a = *b; *b = t; }
 
 void quickSort (int arr[], int n)
 {
-    int i = 0;
-    int j = n-1;
-    int *qi = arr;
-    int *qj = arr + j;
-    int *qp = qj;
-    
     while ( n > 1)
-    {
+    {    
+        int i = 0;
+        int j = n-1;
+        int *qi = arr;
+        int *qj = arr + j;
+        int *qp = qj;
+    
+
         while (i < j)
         {
             while (i < j && *qi <= *qp)
-               i++, qi++;
-           while (i < j && *qj >= *qp)
-               j--, qj--;
+                i++, qi++;
+            while (i < j && *qp <= *qj)
+                j--, qj--;
         
-           if (i < j)
-           {
-               swap(qi, qj);
-               i++, qi++;
-           }
+            if (i < j)
+            {
+                swap(qi, qj);
+                i++, qi++;
+            }
         }  
         if (qi != qp)
             swap(qi, qp);
@@ -55,6 +56,6 @@ int main()
     
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
-
+  
     return 0;
 }
