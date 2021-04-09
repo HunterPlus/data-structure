8478c66c48bc2006089ed54d9a6edcf4ad70c39e
HunterPlus <zlw08140957@gmail.com>
2021-04-09 17:19:08 +0800

    Update CpyCmpSub.c

--- string/CpyCmpSub.c
+++ string/CpyCmpSub.c
@@ -39,13 +39,21 @@ char *findSub (char *s1, char *s2)
     return NULL;
 }
 
-char* cpyString (char *s1, char *s2)
+char* Stringcpy (char *s1, char *s2)
 {
     char *s;
     
     for (s = s1; (*s++ = *s2++); ) ;
     
     return s1;
+}
+char *stringcpy1 (char *dest, const char *src)
+{
+	char *tmp = dest;
+
+	while ((*dest++ = *src++) != '\0') ;
+	
+	return tmp;
 }
 
 int stringCmp (char *s1, char *s2)
@@ -70,7 +78,16 @@ int stringCmp1 (char *s1, char *s2)
     else
         return (*s1 > *s2)? 1 : -1;
 }
+int strcasecmp(const char *s1, const char *s2)
+{
+	int c1, c2;
 
+	do {
+		c1 = tolower(*s1++);
+		c2 = tolower(*s2++);
+	} while (c1 == c2 && c1 != 0);
+	return c1 - c2;
+}
 int main()
 {
     char *s2 = "Hello World!";
