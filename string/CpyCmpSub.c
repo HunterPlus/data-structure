8b8b80f91911b15634208bae088508a2f2b8c2ae
HunterPlus <zlw08140957@gmail.com>
2021-04-09 17:54:57 +0800

    Update CpyCmpSub.c

--- string/CpyCmpSub.c
+++ string/CpyCmpSub.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
+char *findFirst (char *s, char c);
+char *findLast (char *s, char c);
+char *findSub (char *s1, char *s2);
+char *stringcpy (char *dest, const char *src);
+char *stringncpy(char *dest, const char *src, size_t count);
+char *strcat(char *dest, const char *src);
+int stringCmp (char *s1, char *s2);
+int strcasecmp(const char *s1, const char *s2);
 
 char *findFirst (char *s, char c)
 {
-    for (; *s != c; s++)
-        if (*s == '\0')
-            return NULL;
-        
-    return s;
+	while (*s && *s != (char)c)
+		s++;
+	return (char *)s;
 }
 char *findLast (char *s, char c)
 {
-    char *sc;
-    
-    for (sc = NULL; ; s++)
-    {
-        if (*s == c)
-            sc = s;
-        
-        if (*s == '\0')
-            return sc;
-    }
+	const char *last = NULL;
+	do {
+		if (*s == (char)c)
+			last = s;
+	} while (*s++);
+	return (char *)last;
 }
 char *findSub (char *s1, char *s2)
 {
@@ -38,16 +40,7 @@ char *findSub (char *s1, char *s2)
     
     return NULL;
 }
-
-char* Stringcpy (char *s1, char *s2)
-{
-    char *s;
-    
-    for (s = s1; (*s++ = *s2++); ) ;
-    
-    return s1;
-}
-char *stringcpy1 (char *dest, const char *src)
+char *stringcpy (char *dest, const char *src)
 {
 	char *tmp = dest;
 
@@ -77,16 +70,8 @@ char *strcat(char *dest, const char *src)
 		;
 	return tmp;
 }
-int stringCmp (char *s1, char *s2)
-{
-    for (; (*s1 == *s2); s1++,s2++)
-        if (*s1 == '\0')
-            return 0;
-            
-    return (*s1 > *s2) ? 1 : -1;
-}
 
-int stringCmp1 (char *s1, char *s2)
+int stringCmp (char *s1, char *s2)
 {
     while ((*s1 && *s2) && *s1 == *s2)
     {
