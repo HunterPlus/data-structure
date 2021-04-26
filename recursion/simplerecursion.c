66683ef24eb7a9d76a4c534339e714f62c128758
HunterPlus <zlw08140957@gmail.com>
2021-04-26 21:39:01 +0800

    Create simplerecursion.c

---
+++ recursion/simplerecursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void printFun(int test)
+{
+    if (test < 1)
+        return;
+    else {
+        printf("%d ", test);
+        
+        printFun(test - 1); 
+        
+        printf("%d ", test);
+        
+        return;
+    }
+}
+ 
+
+int main()
+{
+    int test = 3;
+    printFun(test);
+    
+    return 0;
+}
