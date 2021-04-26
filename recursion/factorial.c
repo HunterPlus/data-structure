b4ec21cc0d79f9842a8d8404789cd417f94b06c7
HunterPlus <zlw08140957@gmail.com>
2021-04-26 21:53:33 +0800

    Create factorial.c

---
+++ recursion/factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int f(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    
+    else
+        return n * f(n - 1);
+}
+ 
+int main()
+{
+    int n = 5;
+    printf("factorial of %d is: %d", n, f(n));
+    return 0;
+}
