49a721c9d523589f267c957eee147099192b3ddc
HunterPlus <zlw08140957@gmail.com>
2021-04-30 21:25:52 +0800

    Update lis_dp.c

--- dp/lis_dp.c
+++ dp/lis_dp.c
@@ -2,22 +2,27 @@
 
 int lis (int a[], int n)
 {
+    int max = 1, end = 0;
     int dp[n];
-    
-    for (int i = 0; i < n; i++)
-        dp[i] = 1;
-    
+
+    dp[0] = 1;
+
     for (int i = 1; i < n; i++)
     {
+        dp[i] = 1;
+
         for (int j = 0; j < i; j++)
-        {
             if (a[i] > a[j] && dp[j] + 1 > dp[i])
-            dp[i] = dp[j] + 1;
-        }
+                dp[i] = dp[j] + 1;
+        
+        if (dp[i] > max)
+            end = i, max = dp[i];
     }
-    int max = dp[0];
+
     for (int i = 0; i < n; i++)
-        max = (dp[i] > max)? dp[i] : max;
+        printf("%2d ", dp[i]);
+
+    printf("\nidx end: %d\tLIS: %d ", end, max);
 
     return max;
 }
@@ -27,7 +32,7 @@ int main()
     int arr[] = {5, 11, 3, 15, 11, 25, 20, 30, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
     
-    printf("\nLIS is : %d \n", lis(arr, n));
+    lis(arr, n);
 
     return 0;
 }
