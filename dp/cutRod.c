71528aaa06c3499328ac1077aef096c1c2c9dbe5
HunterPlus <zlw08140957@gmail.com>
2021-04-30 08:17:05 +0800

    Update cutRod.c

--- dp/cutRod.c
+++ dp/cutRod.c
@@ -8,11 +8,11 @@ int cutRod (int p[], int n)
     if (n <= 0)
         return 0;
     
-    int max_val = INT_MIN;
+    int q = INT_MIN;
     for (int i = 0; i < n; i++)
-        max_val = max(max_val, p[i] + cutRod(p, n-i-1));
+        q = max(q, p[i] + cutRod(p, n-i-1));
     
-    return max_val;
+    return q;
 }
 
 int main()
