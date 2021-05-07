bb814c468758168a9a94d56aa61acd8fb64fda2f
HunterPlus <zlw08140957@gmail.com>
2021-05-07 09:02:45 +0800

    Update Warnsdorff_knighttour.c

--- backtracking/Warnsdorff_knighttour.c
+++ backtracking/Warnsdorff_knighttour.c
@@ -1,108 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define N 8
+#define M 8
 
-static int cx[N] = {1, 1, 2, 2, -1, -1, -2, -2};
-static int cy[N] = {2, -2, 1, -1, 2, -2, 1, -1};
+int dx[M] = {1,1,2,2,-1,-1,-2,-2};
+int dy[M] = {2,-2,1,-1,2,-2,1,-1};
 
-int limits(int x, int y)
+int safe (int a[], int x, int y)
 {
-    return ((x >= 0 && y >= 0) && (x < N && y < N));
+    int e = a[x * N + y];
+    if (x >= 0 && x < N && y >= 0 && y < N)
+        return e == -1;
+    return 0;
 }
-int isempty(int a[], int x, int y)
+int getdegree (int a[], int x, int y)
 {
-    return limits(x, y) && (a[x * N + y] < 0);
+    int deg = 0;
+    for (int i = 0; i < M; i++)
+        if (safe(a, x+dx[i], y+dy[i]))
+            deg++;
+    return deg;
 }
-int getdegree(int a[], int x, int y)
+int nextmove (int a[], int *x, int *y)
 {
-    int count = 0;
-    for (int i = 0; i < N; i++)
-        if (isempty(a, x + cx[i], y + cy[i]))
-            count++;
-
-    return count;
-}
-int nextmove(int a[], int *x, int *y)
-{
-    int min_deg_idx = -1;
-    int min_deg = N + 1;
-    int nx, ny, c;
-
-    int start = rand() % N;
-    for (int count = 0; count < N; count++)
+    int idx = -1;
+    int deg = M+1;
+    
+    int nx, ny;
+    int start = rand() % M;
+    for (int count = 0; count < M; count++)
     {
-        int i = (start + count) % N;
-        nx = *x + cx[i];
-        ny = *y + cy[i];
-        if (isempty(a, nx, ny) &&
-            (c = getdegree(a, nx, ny)) < min_deg)
+        int i = (start + count) % M;
+        nx = *x + dx[i];
+        ny = *y + dy[i];
+        
+        int c;
+        if (safe(a, nx, ny) && (c = getdegree(a, nx, ny)) < deg)
         {
-            min_deg_idx = i;
-            min_deg = c;
+            idx = i;
+            deg = c;
         }
     }
-    if (min_deg_idx == -1)
+    if (idx == -1)
         return 0;
-
-    nx = *x + cx[min_deg_idx];
-    ny = *y + cy[min_deg_idx];
-
-    a[nx * N + ny] = a[(*x) * N + (*y)] + 1;
-
+        
+    nx = *x + dx[idx];
+    ny = *y + dy[idx];
+    
+    a[nx * N + ny] = a[(*x) * N + *y] + 1;
+    
     *x = nx;
     *y = ny;
-
+    
     return 1;
 }
-
-void print(int a[])
+int neighbour (int x, int y, int xx, int yy)
 {
-    for (int i = 0; i < N; ++i)
-    {
-        for (int j = 0; j < N; ++j)
-            printf("%d\t", a[i * N + j]);
-        printf("\n");
-    }
-}
-int neighbour(int x, int y, int xx, int yy)
-{
-    for (int i = 0; i < N; i++)
-        if ((x + cx[i] == xx) && (y + cy[i] == yy))
+    for (int i = 0; i < M; i++)
+        if ((x+dx[i] == xx) && (y+dy[i] == yy))
             return 1;
     return 0;
 }
-int findclosedtour()
+void print2D(int a[], int n)
 {
-    int a[N * N];
-    for (int i = 0; i < N * N; i++)
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            printf("%d\t",a[i*n + j]);
+        printf("\n");
+    }
+}
+int findclosedtour (int a[])
+{
+    for (int i = 0; i < N*N; i++)
         a[i] = -1;
-
+        
     int sx = rand() % N;
     int sy = rand() % N;
-
-    int x = sx, y = sy;
-    a[x*N + y] = 1;
-
-    for (int i = 0; i < N*N - 1; i++)
+    
+    int x = sx;
+    int y = sy;
+    a[x * N + y] = 1;
+    for (int i = 0; i < N*N-1; i++)
         if (nextmove(a, &x, &y) == 0)
             return 0;
-        
-    if (!neighbour(x, y, sx, sy))
+    if (neighbour(x, y, sx, sy) == 0)
         return 0;
-
-    print(a);
+        
+    printf("start point: %d  %d\n", sx, sy);
+    print2D(a, N);
     return 1;
 }
-
-int main()
+void knighttour ()
 {
+    int a[N*N];
+    
     srand(time(NULL));
-
-    while (!findclosedtour())
+    while (!findclosedtour(a))
     {
         ;
     }
+}
+int main()
+{
+    knighttour();
 
     return 0;
 }
