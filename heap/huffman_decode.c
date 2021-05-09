887083b150663447bc85dd90b7fae5685021522b
HunterPlus <zlw08140957@gmail.com>
2021-05-09 09:51:25 +0800

    Update huffman_decode.c

--- heap/huffman_decode.c
+++ heap/huffman_decode.c
@@ -10,7 +10,6 @@ struct huffcode
     int freq;
     char *codes;
 };
-static struct huffcode hufftable[HL] = (struct huffcode){0, 0, NULL};
 struct node
 {
     char ch;
@@ -23,7 +22,11 @@ struct heap
     int cap;
     struct node **arr;
 };
+
+static struct huffcode hufftable[HL] = (struct huffcode){0, 0, NULL};
 /*******************************************************************/
+void huffmancodes (char *msg);
+
 struct node *newnode (char ch, int freq);
 struct heap *createheap (int capacity);
 void swapnode (struct node **a, struct node **b);
@@ -33,12 +36,45 @@ void insertheap (struct heap *hp, struct node *nd);
 void buildheap (struct heap *hp);
 struct heap *createandbuildheap (char data[], int freq[], int n);
 struct node *huffmantree (char data[], int freq[], int n);
-void huffmancodes (char data[], int freq[], int n);
+
+void formatmsg (char *msg, char **chs, int **freq, int *nodes);
+void storecodes(struct node *root, char *codebuffer, int top, struct huffcode *hfc);
+void printhuffcodes (char *codes);
 void printcodes (struct node *root, int arr[], int top);
 void printarray (int arr[], int n);
 
 
 /********************************************************************/
+void huffmancodes1 (char data[], int freq[], int n)
+{
+    struct node *root = huffmantree(data, freq, n);
+    int arr[HEIGHT];
+    int top = 0;
+    
+    printcodes(root, arr, top);
+}
+void huffmancodes (char *msg)
+{
+    char *chs;
+    int *freq, n;
+    formatmsg(msg, &chs, &freq, &n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        int idx = chs[i];
+        hufftable[idx].ch = 1;
+        hufftable[idx].freq = freq[i];
+    }
+    
+    struct node *root = huffmantree(chs, freq, n);
+    free(chs);
+    free(freq);
+    
+    char codebuffer[HL] = { 0 };
+    int top = 0;
+    storecodes(root, codebuffer, top, hufftable);    
+}
+
 struct node *newnode (char ch, int freq)
 {
     struct node *t = (struct node*)malloc(sizeof(struct node));
@@ -142,13 +178,64 @@ struct node *huffmantree (char data[], int freq[], int n)
     return exactheap(hp);
 }
 
-void huffmancodes (char data[], int freq[], int n)
+void formatmsg (char *msg, char **chs, int **freq, int *nodes)
 {
-    struct node *root = huffmantree(data, freq, n);
-    int arr[HEIGHT];
-    int top = 0;
+    int ht[128] = { 0 };
+    int msglength, n;
     
-    printcodes(root, arr, top);
+    int i, j, c;
+    for (i = 0; (c = msg[i]) != '\0'; i++)
+        ht[c] += 1;
+    msglength = i;
+    
+    for (n = 0, i = 0; i < 128; i++)
+        if (ht[i] != 0)
+            n += 1;
+    *nodes = n;
+            
+    *chs = (char *)malloc((n+1) * sizeof(char));
+    *freq = (int *)malloc(n * sizeof(int));
+    for (j = 0, i = 0; i < 128; i++)
+    {
+        if (ht[i] != 0)
+        {
+            (*chs)[j] = i;
+            (*freq)[j] = ht[i];
+            j++;
+        }
+    }
+    (*chs)[j] = '\0';
+    
+}
+void storecodes(struct node *root, char *codebuffer, int top, struct huffcode *hfc)
+{
+    if (root->l)
+    {
+        codebuffer[top] = '0';
+        storecodes(root->l, codebuffer, top+1, hfc);
+    } 
+    if (root->r)
+    {
+        codebuffer[top] = '1';
+        storecodes(root->r, codebuffer, top+1, hfc);
+    }
+    if (!root->l && !root->r)
+    {
+        char c = root->ch;
+        int l;
+        for (l = 0; codebuffer[l] != '\0'; l++);
+        
+        char *codes = (char *)malloc((l+1) * sizeof(char));
+        for (int i = 0; i < l; i++)
+            codes[i] = codebuffer[i];
+        codes[i] = '\0';
+        
+        hfc[c].codes = codes;
+        printhuffcodes(codes);
+}
+void printhuffcodes (char *codes)
+{
+    printf("%s\n", codes);
 }
 void printcodes (struct node *root, int arr[], int top)
 {
