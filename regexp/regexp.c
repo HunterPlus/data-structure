1c4a2f4676845f9cfa16e60e776814fe6a0c7921
HunterPlus <zlw08140957@gmail.com>
2022-04-27 11:52:22 +0800

    Update regexp.c

--- regexp/regexp.c
+++ regexp/regexp.c
@@ -1,3 +1,12 @@
+/*
+ * 	c	matches any literal character c
+ * 	.	matches any single character
+ * 	^    	matches the beginning of the input string
+ * 	$    	matches the end of the input string
+ * 	*    	matches zero or more occurrences of the previous character
+ */
+
+
 int matchhere(char *, char *);
 int matchstar(int, char *, char *);
 
