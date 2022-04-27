472ce2f11e47a3abf2cc7077d7d3426c67ce791a
HunterPlus <zlw08140957@gmail.com>
2022-04-27 11:07:28 +0800

    Update regexp.c

--- regexp/regexp.c
+++ regexp/regexp.c
@@ -1,7 +1,9 @@
 /* match:search for regexp anywhere in text */
 int match(char *regexp, char *test)
 {
-	if (regexp[0] == '^)
-	    return matchhere(regexp + 1, text);
-	    
+	if (regexp[0] == '^')
+		return matchhere(regexp + 1, text);
+	do {		/* must look even string is empty	*/
+		if (matchhere(regexp, text))
+	}
 }
