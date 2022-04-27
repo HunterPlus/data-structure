47b4899a924c0bcc596c742332ffa3ea6fc73249
HunterPlus <zlw08140957@gmail.com>
2022-04-27 16:22:38 +0800

    Update useqsort.c

--- clibray/useqsort.c
+++ clibray/useqsort.c
@@ -8,7 +8,7 @@
  * void qsort(void *base, size_t nel, size_t width, int (*compar)(const void *, const void *));
  */
 
-/* scmp: string compare of *s1 and *s2 */
+/* scmp: string compare of *p1 and *p2 */
 int scmp(void *p1, void *p2)
 {
 	char	*s1, *s2;
@@ -18,9 +18,25 @@ int scmp(void *p1, void *p2)
 	return strcmp(s1, s2);
 }
 
+/* icmp: integer compare of *p1 and *p2	*/
+int icmp(void *p1, void *p2)
+{
+	int	v1, v2;
+	
+	v1 = *(int *)p1;
+	v2 = *(int *)p2;
+	if (v1 < v2)
+		return -1;
+	else if (v1 > v2)
+		return 1;
+	return 0;
+}
+
 int main()
 {
 	char	*str[N];	/* array [N] of pointers to string */
+	int	arr[N];
 	
 	qsort(str, N, sizeof(str[0]), scmp);
+	qsort(arr, N, sizeof(arr[0]), icmp);
 }
